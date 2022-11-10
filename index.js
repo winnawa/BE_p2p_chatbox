cd35033f0733002b3e1ed19bbfd2838c694173cb
Nam Khoa <khoa.pham28042002@hcmut.edu.vn>
2022-11-10 16:17:03 +0700

    my first commit

---
+++ index.js
@@ -0,0 +1,112 @@
+import express from 'express'
+import { ObjectId } from 'mongodb';
+import { client, createNewFriendship, createNewUser, getFriendListOfUser, updateSingleUser } from './mongodb/index.js';
+import { listAllUsers } from './mongodb/index.js';
+const app = express()
+
+app.use(express.json())
+app.use(express.urlencoded({extended:false}))
+
+const main = async() => {
+    await client.connect()
+}
+main()
+.then(
+    ()=>{console.log('🎉 connected to database successfully')}
+)
+.catch(error => console.error(error));
+  
+
+app.get('/',(req,res)=>{
+    res.send("hello world")
+
+})
+
+app.get('/getAllUsers', async(req,res)=>{
+    const result = await listAllUsers(client)
+    res.send(result)
+})
+
+app.post('/updateUserStatus', async(req,res)=>{
+    // const result = await listAllUser(client)
+    const client_sent_obj = req.body
+    const obj_Id = client_sent_obj["id"]
+    const newStatus = client_sent_obj["status"]
+    if (obj_Id && newStatus){
+        console.log(obj_Id.length)
+    
+        await updateSingleUser(client,{"id" : obj_Id},{"status": `${newStatus}`})
+        res.send({"status": "update successfully"})
+    }
+    else{
+    // console.log(obj.name)
+    console.log(obj_Id)
+    console.log(newStatus)
+    res.send({"status": "update unsuccessfully"})
+    }
+})
+
+app.post('/getFriendList', async(req,res)=>{
+    // const result = await listAllUser(client)
+    const client_sent_obj = req.body
+    const obj_Id = client_sent_obj["id"]
+    if (obj_Id ){
+        //console.log(obj_Id.length)
+    
+        const result = await getFriendListOfUser(client, {"id": obj_Id})
+        res.send(result)
+    }
+    else{
+    // console.log(obj.name)
+    //console.log(obj_Id)
+    //console.log(newStatus)
+    res.send({"status": "getFriendList unsuccessfully"})
+    }
+})
+
+
+
+app.post('/createFriendship', async(req,res)=>{
+    // const result = await listAllUser(client)
+    const client_sent_obj = req.body
+    const peerID = client_sent_obj["peerID"]
+    const friendID = client_sent_obj["friendID"] 
+    if (peerID && friendID ){
+        //console.log(obj_Id.length)
+        const result = await createNewFriendship(client, peerID, friendID)
+        if (result){
+            res.send({"status":"createFriendship successfully"})
+        }
+        else{
+            res.send({"status" : "createFriendship unsuccessfully"})
+        }
+    }
+    else{
+    // console.log(obj.name)
+    //console.log(obj_Id)
+    //console.log(newStatus)
+    res.send({"status": "createFriendship unsuccessfully"})
+    }
+})
+
+
+app.post('/createNewUser', async(req,res)=>{
+    // const result = await listAllUser(client)
+    const client_sent_obj = req.body
+    const status = client_sent_obj["status"]
+    const address = client_sent_obj["address"]
+    const name = client_sent_obj["name"]
+    const last_name = client_sent_obj["last_name"]
+    if (status && address && name && last_name){
+        await createNewUser(client,{"address": address, "name" : name, "last_name" : last_name, "status" : status})
+        res.send({"status": "create  successfully"})
+    }
+    else{
+    // console.log(obj.name)
+    res.send({"status": "create unsuccessfully"})
+    }
+})
+
+
+
+app.listen(3001, ()=>{console.log("listening on port 3001")})
---
+++ mongodb/index.js
@@ -0,0 +1,104 @@
+import { MongoClient, ObjectId } from "mongodb";
+
+
+const uri = "mongodb+srv://namkhoa:namkhoapro@cluster0.8p7d3.mongodb.net/?retryWrites=true&w=majority";
+export const client = new MongoClient(uri)
+
+
+
+export const listAllUsers = async(client) => {
+    const cursor = client.db("PEERinfo").collection("peerList").find({})
+    const result = await cursor.toArray();
+    return(result)
+}
+
+export const updateSingleUser = async(client,currentUser,newProps) => {
+    let userID= ObjectId(currentUser.id)
+    const result = await client.db("PEERinfo").collection("peerList").updateOne({"_id" : userID},
+    {$set:newProps})
+    // const result = await client.db("PEERinfo").collection("peerList").updateOne(currentUser,
+    // {$set:newProps})
+    console.log(result.matchedCount)
+    console.log(result.modifiedCount)
+}
+
+export const createNewUser = async(client, newUser)=>{
+    const result = await client.db("PEERinfo").collection("peerList").insertOne(newUser)
+    return true
+}
+
+
+export const createNewFriendship = async(client,currentUserId,friendOfUserId)=>{
+    const userID = currentUserId
+    const friendID = friendOfUserId
+    const user = await client.db("PEERinfo").collection("peerList").findOne({"_id":ObjectId(userID)})
+    const friend = await client.db("PEERinfo").collection("peerList").findOne({"_id":ObjectId(friendID)})
+    if (user && friend){
+        const friendship1 = await client.db("PEERinfo").collection("peerToPeerFriendship").findOne({"peerId":userID, "friendPeerId":friendID})
+        if (!friendship1){
+            const result1 = await client.db("PEERinfo").collection("peerToPeerFriendship").insertOne({
+                peerId : userID,
+                friendPeerId : friendID
+            })
+        }
+        const friendship2 = await client.db("PEERinfo").collection("peerToPeerFriendship").findOne({"peerId":friendID, "friendPeerId": userID})
+        if (!friendship2){
+            const result2 = await client.db("PEERinfo").collection("peerToPeerFriendship").insertOne({
+                peerId : friendID,
+                friendPeerId : userID
+            })
+        }
+        return true
+    }
+    return false
+}
+
+export const getFriendListOfUser = async(client,currentUser)=>{
+    const userID = currentUser.id
+    const cursor = client.db("PEERinfo").collection("peerToPeerFriendship").find({peerId:userID})
+    const relationshipResults = await cursor.toArray()
+    // console.log(relationshipResults)
+    const friendListReal = []
+    const friendList = relationshipResults.map(async (element, index)=>{
+        // console.log('In side map func')
+        const friendPeerId = ObjectId(element.friendPeerId)
+        // console.log(friendPeerId)
+        const result = await client.db("PEERinfo").collection("peerList").findOne({"_id" : friendPeerId})
+        //console.log(result)
+        friendListReal.push(result)
+        //console.log("after adding",friendListReal)
+        return result
+    })
+    
+    return Promise.all(friendList).then(()=>{ return friendListReal })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// export const connectAndAction= async(client,action,payload) =>{
+//     try{
+//         await client.connect()
+//     // await createNewUser(client,{
+//     //     address : "214 Ly Thuong Kiet",
+//     //     name : "Lap",
+//     //     last_name : "Nguyen Dinh Gia",
+//     //     status : "offline"
+//     // })
+//     }
+//     catch(e){
+//         console.log(e)
+//     }
+//     finally{
+//         await client.close()
+//     }
+// }
